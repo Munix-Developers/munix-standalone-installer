cfd079fcb8193c6a53706a751d920f7d7aaaeefa
MatMercer <mateusmercer@gmail.com>
2020-04-12 17:04:00 -0300

    Add InstallConfig json structure

--- installer.go
+++ installer.go
@@ -1,8 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 }
 
+type InstallConfig struct {
+	Version      string             `json:"version"`
+	Keyboard     KeyboardConfig     `json:"keyboard"`
+	Localization LocalizationConfig `json:"localization"`
+	Computer     ComputerConfig     `json:"computer"`
+	Storage      StorageConfig      `json:"storage"`
+}
+
+type KeyboardConfig struct {
+	Layout string `json:"layout"`
+}
+
+type LocalizationConfig struct {
+	Language string   `json:"language"`
+	Locales  []string `json:"locales"`
+	Timezone string   `json:"timezone"`
+}
+
+type ComputerConfig struct {
+	Hostname string `json:"hostname"`
+	Username string `json:"username"`
+	Password string `json:"Password"`
+}
+
+type StorageConfig struct {
+	Devices []DeviceConfig `json:"devices"`
+}
+
+type DeviceConfig struct {
+	Device     string            `json:"device"`
+	Partitions []PartitionConfig `json:"partitions"`
+}
+
+type PartitionConfig struct {
+	Type  string `json:"type"`
+	Mount string `json:"mount"`
+	Start uint64 `json:"start"`
+	End   uint64 `json:"end"`
+}
+
+func ReadConfig(bytes []byte, config *InstallConfig) error {
+	return json.Unmarshal(bytes, config)
+}
---
+++ installer_test.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfigJsonLoad(t *testing.T) {
+	a := assert.New(t)
+
+	var config InstallConfig
+	err := ReadConfig([]byte(validJson), &config)
+
+	a.NoError(err)
+	a.Equal("version", config.Version)
+	a.Equal("layout", config.Keyboard.Layout)
+
+	a.Equal("language", config.Localization.Language)
+	a.Equal("locale", config.Localization.Locales[0])
+	a.Equal("locale", config.Localization.Locales[1])
+	a.Equal("timezone", config.Localization.Timezone)
+
+	a.Equal("hostname", config.Computer.Hostname)
+	a.Equal("username", config.Computer.Username)
+	a.Equal("password", config.Computer.Password)
+
+	a.Equal("device", config.Storage.Devices[0].Device)
+	a.Equal("type", config.Storage.Devices[0].Partitions[0].Type)
+	a.Equal("mount", config.Storage.Devices[0].Partitions[0].Mount)
+	a.Equal(uint64(777), config.Storage.Devices[0].Partitions[0].Start)
+	a.Equal(uint64(777), config.Storage.Devices[0].Partitions[0].End)
+	a.Equal("type", config.Storage.Devices[0].Partitions[1].Type)
+	a.Equal("mount", config.Storage.Devices[0].Partitions[1].Mount)
+	a.Equal(uint64(777), config.Storage.Devices[0].Partitions[1].Start)
+	a.Equal(uint64(777), config.Storage.Devices[0].Partitions[1].End)
+
+	a.Equal("device", config.Storage.Devices[1].Device)
+	a.Equal("type", config.Storage.Devices[1].Partitions[0].Type)
+	a.Equal("mount", config.Storage.Devices[1].Partitions[0].Mount)
+	a.Equal(uint64(777), config.Storage.Devices[1].Partitions[0].Start)
+	a.Equal(uint64(777), config.Storage.Devices[1].Partitions[0].End)
+}
+
+var validJson = `
+{
+  "version": "version",
+  "keyboard": {
+    "layout": "layout"
+  },
+  "localization": {
+    "language": "language",
+    "locales": [
+      "locale",
+      "locale"
+    ],
+    "timezone": "timezone"
+  },
+  "computer": {
+    "hostname": "hostname",
+    "username": "username",
+    "password": "password"
+  },
+  "storage": {
+    "devices": [
+      {
+        "device": "device",
+        "partitions": [
+          {
+            "type": "type",
+            "mount": "mount",
+            "start": 777,
+            "end": 777
+          },
+          {
+            "type": "type",
+            "mount": "mount",
+            "start": 777,
+            "end": 777
+          }
+        ]
+      },
+      {
+        "device": "device",
+        "partitions": [
+          {
+            "type": "type",
+            "mount": "mount",
+            "start": 777,
+            "end": 777
+          }
+        ]
+      }
+    ]
+  }
+}
+`
