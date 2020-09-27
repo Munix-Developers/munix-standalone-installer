d47aaddd77591b2949641faaf989b88a035144e3
MatMercer <contact@matbm.net>
2020-09-27 01:13:06 -0300

    Enhance workflow files

--- workflow/virtualbox.sh
+++ workflow/virtualbox.sh
@@ -4,9 +4,14 @@
 # Run host-webserver.sh first.
 
 # Run this command inside the machine:
-# export MUNIX=HOST_WEBSERVER_IP:7777 && curl $MUNIX/virtualbox.sh | sh -
+# export M=HOST_IP && curl $M:7777/v | sh -
 
-curl $MUNIX/valid.json > valid.json && \
-curl $MUNIX/muinstaller > muinstaller && \
-chmod +x muinstaller && \
-./muinstaller valid.json
+HOST_MUNIX_IP=$M
+HOST_MUNIX_PORT=7777
+HOST_MUNIX_SQUID_PORT=8080
+export all_proxy="$HOST_MUNIX_IP:$HOST_MUNIX_SQUID_PORT"
+
+curl "http://$HOST_MUNIX_IP:$HOST_MUNIX_PORT"/valid.json >valid.json &&
+  curl "http://$HOST_MUNIX_IP:$HOST_MUNIX_PORT"/muinstaller >muinstaller &&
+  chmod +x muinstaller &&
+  ./muinstaller valid.json
